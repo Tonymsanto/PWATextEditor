9e18792d71e44d09e71d358af51bfb0975a9f21c
Anthony Santo <tonymsanto@gmail.com>
2022-10-21 18:06:44 -0600

    Was able to install and save on web browser

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
+        template: "./index.html",
+        // Optional parameters
+        title: "Text Editor",
+      }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just another text editor",
+        short_name: "JATE",
+        description: "Just another text editor!",
+        background_color: "#1e1e1e",
+        theme_color: "#1e1e1e",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          // This looks for a .css file and adds this into the bundle.js file
+          test: /\.css$/i,
+          // Additional modules for converting the css into js
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Add babel-loader to webpack in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
